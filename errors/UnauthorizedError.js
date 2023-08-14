bdf094d8e8071254cc8fc2ac29649e896883a986
Alina Martynenko <martynenko.av.94@mail.ru>
2023-08-14 19:57:17 +1000

    Добавить ссылку на репозиторий

--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -1,7 +1,7 @@
 class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'AuthError';
+    this.name = 'UnauthorizedError';
     this.statusCode = 401;
   }
 }
